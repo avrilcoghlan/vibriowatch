85b109258bb187d316688f63833bbcb92546f803
Avril Coghlan <a.coghlan@ucc.ie>
2023-02-27 10:55:11 +0000

    Update conf.py
    Updated project name and author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Vibriowatch'
+copyright = '2023, Avril Coghlan'
+author = 'Avril Coghlan'
 
 release = '0.1'
 version = '0.1.0'
